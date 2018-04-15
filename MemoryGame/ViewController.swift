db26581b30a219598d52bf7c1511d2f20a1495b1
Jason <jasonccmfry@gmail.com>
2018-04-14 21:26:03 -0700

    Initial Commit

---
+++ MemoryGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemoryGame
+//
+//  Created by Jason Chen on 2018-04-14.
+//  Copyright © 2018 Jason Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
